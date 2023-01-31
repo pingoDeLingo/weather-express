46f2797119ac4fd34db4c3a68f8d85121727ad12
Robin <robin.noormets@voco.ee>
2023-01-31 11:11:25 +0200

    Created form request responce with API data

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const path = require("path")
+const fetch = require("node-fetch")
+const {response} = require("express");
 
-const path = require('path')
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
-app.get('/', function (req, res){
-    res.render('index')
+const key = "b9b27e0c6774d5bc94269cbec554bdbe"
+let city = "Tartu"
+
+app.get("/", (req, res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+
+            res.render("index", {
+                description: description,
+                city: city,
+                temp: temp,
+            })
+        })
+})
+
+
+app.post('/', function (req, res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then((response) => {
+        return response.json()
+    })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
 })
 
 app.listen(3005)
